40df095e08bd30540991cd9d9b488ac264da627a
zfy <zfy0098@163.com>
2017-01-06 15:52:50 +0800

    添加多个收件人和抄送人

--- src/com/rom/util/email/MailBean.java
+++ src/com/rom/util/email/MailBean.java
@@ -1,89 +1,111 @@
-package com.rom.util.email;
+package com.email;
 
 import java.util.Vector;
+
 public class MailBean {
-	  private String to;                                // 收件人
-	    private String from;                            // 发件人
-	    private String host;                            // SMTP主机
-	    private String username;                        // 发件人的用户名
-	    private String password;                        // 发件人的密码
-	    private String subject;                            // 邮件主题
-	    private String content;                            // 邮件正文
-	    Vector<String> file;                            // 多个附件
-	    private String filename;                        // 附件的文件名
-	    
-	    public String getTo() {
-	        return to;
-	    }
+	Vector<String> to; // 收件人
+	private String from; // 发件人
+	Vector<String> copyColumn;	// 抄送人
+	private String host; // SMTP主机
+	private String username; // 发件人的用户名
+	private String password; // 发件人的密码
+	private String subject; // 邮件主题
+	private String content; // 邮件正文
+	Vector<String> file; // 多个附件
+	private String filename; // 附件的文件名
 
-	    public void setTo(String to) {
-	        this.to = to;
-	    }
 
-	    public String getFrom() {
-	        return from;
-	    }
+	
+	
+	
 
-	    public void setFrom(String from) {
-	        this.from = from;
-	    }
+	public Vector<String> getTo() {
+		return to;
+	}
 
-	    public String getHost() {
-	        return host;
-	    }
+	public void setTo(String toName) {
+		if(to==null){
+			to = new Vector<String>();
+		}
+		to.addElement(toName);
+	}
 
-	    public void setHost(String host) {
-	        this.host = host;
-	    }
+	public Vector<String> getCopyColumn(){
+		return copyColumn;
+	}
+	
+	public void setCopyColumn(String ccName){
+		if(copyColumn==null){
+			copyColumn = new Vector<String>();
+		}
+		copyColumn.addElement(ccName);
+	}
+	
+	public String getFrom() {
+		return from;
+	}
 
-	    public String getUsername() {
-	        return username;
-	    }
+	public void setFrom(String from) {
+		this.from = from;
+	}
 
-	    public void setUsername(String username) {
-	        this.username = username;
-	    }
+	public String getHost() {
+		return host;
+	}
 
-	    public String getPassword() {
-	        return password;
-	    }
+	public void setHost(String host) {
+		this.host = host;
+	}
 
-	    public void setPassword(String password) {
-	        this.password = password;
-	    }
+	public String getUsername() {
+		return username;
+	}
 
-	    public String getSubject() {
-	        return subject;
-	    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-	    public void setSubject(String subject) {
-	        this.subject = subject;
-	    }
+	public String getPassword() {
+		return password;
+	}
 
-	    public String getContent() {
-	        return content;
-	    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-	    public void setContent(String content) {
-	        this.content = content;
-	    }
+	public String getSubject() {
+		return subject;
+	}
 
-	    public String getFilename() {
-	        return filename;
-	    }
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
 
-	    public void setFilename(String filename) {
-	        this.filename = filename;
-	    }
+	public String getContent() {
+		return content;
+	}
 
-	    public Vector<String> getFile(){
-	        return file;
-	    }
-	    
-	    public void attachFile(String fileName) {
-	        	if(file == null)
-		            file = new Vector<String>();
-		        file.addElement(fileName);
-	    }
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	public Vector<String> getFile() {
+		return file;
+	}
+
+	public void attachFile(String fileName) {
+		if (file == null)
+			file = new Vector<String>();
+		file.addElement(fileName);
+	}
+
+	
 }
-
--- src/com/rom/util/email/SendMail.java
+++ src/com/rom/util/email/SendMail.java
@@ -1,8 +1,9 @@
-package com.rom.util.email;
+package com.email;
 
-import java.io.UnsupportedEncodingException;  
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Properties;
 import java.util.Vector;
 
@@ -22,83 +23,93 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 
 
+import com.sun.org.apache.xml.internal.security.utils.Base64;
 
-/**
- *     邮件工具类  
- * @author zfy
- * 		需要jar 包
- * 			activation.jar
- * 			mail.jar
- *
- */
 public class SendMail {
+	public boolean sendMail(MailBean mb) {
+		String host = mb.getHost();
+		final String username = mb.getUsername();
+		final String password = mb.getPassword();
+		String from = mb.getFrom();
+//		String to = mb.getTo();
+		String subject = mb.getSubject();
+		String content = mb.getContent();
+		String fileName = mb.getFilename();
+		Vector<String> file = mb.getFile();
 
-    public boolean sendMail(MailBean mb) {
-        String host = mb.getHost();
-        final String username = mb.getUsername();
-        final String password = mb.getPassword();
-        String from = mb.getFrom();
-        String to = mb.getTo();
-        String subject = mb.getSubject();
-        String content = mb.getContent();
-        String fileName = mb.getFilename();
-        Vector<String> file = mb.getFile();
-      
-        
-        Properties props = System.getProperties();
-        props.put("mail.smtp.host", host);                // 设置SMTP的主机
-        props.put("mail.smtp.auth", "true");            // 需要经过验证
-        
-        Session session = Session.getInstance(props, new Authenticator() {
-            public PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
-            }
-        });
+		Properties props = System.getProperties();
+		props.put("mail.smtp.host", host); // 设置SMTP的主机
+		props.put("mail.smtp.auth", "true"); // 需要经过验证
 
-        try {
-            MimeMessage msg = new MimeMessage(session);
-            msg.setFrom(new InternetAddress(from));
-            InternetAddress[] address = {new InternetAddress(to)};
-            msg.setRecipients(Message.RecipientType.TO, address);
-            msg.setSubject(subject);
+		Session session = Session.getInstance(props, new Authenticator() {
+			public PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		});
 
+		try {
+			MimeMessage msg = new MimeMessage(session);
+			// 设置多个收件人地址  
+            List<String> list = mb.getTo();  
             
-            //   设置抄送人
-            msg.setRecipients(Message.RecipientType.CC,
-					(Address[]) InternetAddress.parse("zfy0098@163.com"));
-            
-            Multipart mp = new MimeMultipart();
-            MimeBodyPart mbpContent = new MimeBodyPart();
-            mbpContent.setText(content);
-            mp.addBodyPart(mbpContent);
+            String toAddress = getAddress(list);  
+			
+			msg.setFrom(new InternetAddress(from));
+			
+			InternetAddress[] address = InternetAddress.parse(toAddress);  
+			msg.setRecipients(Message.RecipientType.TO, address);
+			
+			msg.setSubject(subject);
 
-            /*    往邮件中添加附件    */
-           if(file!=null){
-        	   Enumeration<String> efile = file.elements();
-               while (efile.hasMoreElements()) {
-                   MimeBodyPart mbpFile = new MimeBodyPart();
-                   fileName = efile.nextElement().toString();
-                   FileDataSource fds = new FileDataSource(fileName);
-                   mbpFile.setDataHandler(new DataHandler(fds));
-                try {
-					mbpFile.setFileName("=?GBK?B?" + Base64.getEncoder().encode(fds.getName().getBytes("GBK")) + "?=");
-				} catch (UnsupportedEncodingException e) {
-					e.printStackTrace();
-				} 
-                   mp.addBodyPart(mbpFile);
-               }
-               System.out.println("添加成功");
-           }
+			// 设置抄送人
+			if (mb.getCopyColumn() != null) {
+				msg.setRecipients(Message.RecipientType.CC,
+						(Address[]) InternetAddress.parse(getAddress(mb.getCopyColumn())));
+			}
 
-            msg.setContent(mp);
-            msg.setSentDate(new Date());
-            Transport.send(msg);
-            
-        } catch (MessagingException me) {
-            me.printStackTrace();
-            return false;
-        }
-        return true;
-    }
+			Multipart mp = new MimeMultipart();
+			MimeBodyPart mbpContent = new MimeBodyPart();
+			mbpContent.setText(content);
+			mp.addBodyPart(mbpContent);
 
+			/* 往邮件中添加附件 */
+			if (file != null) {
+				Enumeration<String> efile = file.elements();
+				while (efile.hasMoreElements()) {
+					MimeBodyPart mbpFile = new MimeBodyPart();
+					fileName = efile.nextElement().toString();
+					FileDataSource fds = new FileDataSource(fileName);
+					mbpFile.setDataHandler(new DataHandler(fds));
+					try {
+						mbpFile.setFileName("=?GBK?B?"
+								+ Base64.encode(fds.getName().getBytes("GBK"))
+								+ "?=");
+					} catch (UnsupportedEncodingException e) {
+						e.printStackTrace();
+					}
+					mp.addBodyPart(mbpFile);
+				}
+				System.out.println("添加成功");
+			}
+
+			msg.setContent(mp);
+			msg.setSentDate(new Date());
+			Transport.send(msg);
+
+		} catch (MessagingException me) {
+			me.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	
+	
+	public String getAddress(List<String> list){
+		String address = "";
+		for (String string : list) {
+			address += string + ","; 
+		}
+		return address;
+	}
 }
