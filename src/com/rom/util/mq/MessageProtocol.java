52f22c78443c9c75975317ad2b2a775a8bbd06ad
zfy0098 <zfy0098@163.com>
2017-03-09 17:06:19 +0800

    activemq 请求响应

---
+++ src/com/rom/util/mq/MessageProtocol.java
@@ -0,0 +1,14 @@
+package com.rom.util.mq;
+
+public class MessageProtocol {
+    public String handleProtocolMessage(String messageText) {
+        String responseText;
+        if ("MyProtocolMessage".equalsIgnoreCase(messageText)) {
+            responseText = "I recognize your protocol message";
+        } else {
+            responseText = "Unknown protocol message: " + messageText;
+        }
+         
+        return responseText;
+    }
+}
